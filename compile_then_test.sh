5251bdafe7e5fcad2240fae636d3a85d449b7927
genchadt <62498018+genchadt@users.noreply.github.com>
2024-05-11 22:48:43 -0400

    Removed redundant comments

--- compile_then_test.sh
+++ compile_then_test.sh
@@ -3,21 +3,16 @@
 echo
 echo "Compiling..."
 
-# Record the start time
 start=$(date +%s%N)
 
-# Compile the C++ code
 g++ -std=c++11 -static-libgcc -static-libstdc++ -static -pthread -o spinningload spinningload.cpp
 
-# Record the end time
 end=$(date +%s%N)
 
-# Calculate elapsed time in seconds and nanoseconds
 elapsed_ns=$((end - start))
 elapsed_sec=$((elapsed_ns / 1000000000))
 elapsed_ns=$((elapsed_ns % 1000000000))
 
-# Format the time components
 hours=$((elapsed_sec / 3600))
 mins=$(( (elapsed_sec % 3600) / 60))
 secs=$((elapsed_sec % 60))
