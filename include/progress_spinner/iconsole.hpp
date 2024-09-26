81c794362b79b7b63f1e61a6d450c6ce3856923b
genchadt <62498018+genchadt@users.noreply.github.com>
2024-09-25 22:32:05 -0400

    Improved X-platform support

--- include/progress_spinner/iconsole.hpp
+++ include/progress_spinner/iconsole.hpp
@@ -16,64 +16,50 @@
 class IConsole {
     public:
         virtual ~IConsole() = default;
-        virtual void clearLine() const = 0;
+        virtual void clearLine() const {
+            std::cout << "\r\033[K";
+        }
         virtual void showCursor(bool show_flag) const = 0;
 
     protected:
-        IConsole() {
-            setUTF8();
-        }
-
-        /**
-         * \brief Set the console codepage to UTF-8
-         * \details This function sets the console codepage to UTF-8 by using the Windows API.
-         */
-        #ifdef _WIN32
-        void setUTF8() const {
-            try {
-                UINT current_cp = GetConsoleOutputCP();
-                if (current_cp != CP_UTF8) {
-                    if (!SetConsoleOutputCP(CP_UTF8)) {
-                        throw std::runtime_error("Failed to set console codepage to UTF-8");
-                    }
-                }
-            } catch (const std::exception& e) {
-                std::cerr << "Failed to set console codepage to UTF-8: " << e.what() << std::endl;
-            }
-        }
-        #endif
+        IConsole() = default;
 };
 
+#ifdef _WIN32
 /** 
  * \brief Implementation of IConsole for Windows
  * \details This class provides an implementation of IConsole for Windows.
  */
 class WindowsConsole : public IConsole {
     public:
-        WindowsConsole() = default;
-
-        /**
-         * \brief Clear the current line
-         * \details This function clears the current line in the console via ANSI escape codes.
-         */
-        void clearLine() const override {
-            std::cout << "\r\033[K";
+        WindowsConsole() {
+            setUTF8();
+            enableANSISupport();
         }
 
-        /**
-         * \brief Show or hide the cursor
-         * \details This function shows or hides the cursor in the console by using the Windows API.
-         * \param show_flag
-         */
         void showCursor(bool show_flag) const override {
             HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
             CONSOLE_CURSOR_INFO cursor_info;
             GetConsoleCursorInfo(out, &cursor_info);
             cursor_info.bVisible = show_flag;
-            SetConsoleCursorInfo(out, &cursor_info);    
+            SetConsoleCursorInfo(out, &cursor_info);
+        }
+
+    private:
+        void setUTF8() const {
+            SetConsoleOutputCP(CP_UTF8);
+        }
+
+        void enableANSISupport() const {
+            HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+            DWORD mode = 0;
+            GetConsoleMode(out, &mode);
+            mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(out, mode);
         }
 };
 
+#else
 /** 
  * \class UnixConsole
  * \brief Implementation of IConsole for Unix
@@ -83,23 +69,11 @@ class UnixConsole : public IConsole {
     public:
         UnixConsole() = default;
 
-        /**
-         * \brief Clear the current line
-         * \details This function clears the current line in the console via ANSI escape codes.
-         */
-        void clearLine() const override {
-            std::cout << "\r\033[K";
-        }
-
-        /**
-         * \brief Show or hide the cursor
-         * \details This function shows or hides the cursor in the console by using ANSI escape codes.
-         * \param show_flag
-         */
         void showCursor(bool show_flag) const override {
             std::cout << (show_flag ? "\033[?25h" : "\033[?25l");
         }
 };
+#endif
 
 #ifdef _WIN32
 using Console = WindowsConsole;
