9fa2392ac8f64fd1fc01cf87d72d1ab49c3f2ca2
genchadt <62498018+genchadt@users.noreply.github.com>
2024-10-06 07:55:35 -0400

    indentation fix

--- include/progress_spinner/options.hpp
+++ include/progress_spinner/options.hpp
@@ -40,11 +40,10 @@ struct ProgressSpinnerOptions
     std::vector<std::string> char_frames;
     int update_interval_ms;
 
-    ProgressSpinnerOptions(
-        const option::Label& label = option::Label(),
-        const option::CompletedLabel& completed_label = option::CompletedLabel(),
-        const option::SpinnerChars& char_frames = option::SpinnerChars(),
-        const option::UpdateIntervalMs& update_interval_ms = option::UpdateIntervalMs())
+    ProgressSpinnerOptions(const option::Label& label = option::Label(),
+                           const option::CompletedLabel& completed_label = option::CompletedLabel(),
+                           const option::SpinnerChars& char_frames = option::SpinnerChars(),
+                           const option::UpdateIntervalMs& update_interval_ms = option::UpdateIntervalMs())
         : progress_label(label.progress_label),
           completed_label(completed_label.completed_label),
           char_frames(char_frames.spinner_chars),
@@ -58,11 +57,10 @@ struct HProgressBarOptions {
     std::string empty_char;
     std::string filled_char;
 
-    HProgressBarOptions(
-        const option::Label& label = option::Label(),
-        const option::CompletedLabel& completed_label = option::CompletedLabel(),
-        const option::NumOfSegments& segments = option::NumOfSegments{30},
-        const option::ProgressBarChars& chars = option::ProgressBarChars())
+    HProgressBarOptions(const option::Label& label = option::Label(),
+                        const option::CompletedLabel& completed_label = option::CompletedLabel(),
+                        const option::NumOfSegments& segments = option::NumOfSegments{30},
+                        const option::ProgressBarChars& chars = option::ProgressBarChars())
         : progress_label(label.progress_label),
           completed_label(completed_label.completed_label),
           total_segments(segments.number_of_segments),
@@ -77,10 +75,9 @@ struct VProgressBarOptions
     std::string completed_label;
     std::vector<std::string> char_frames;
 
-    VProgressBarOptions(
-        const option::Label& label = option::Label(),
-        const option::CompletedLabel& completed_label = option::CompletedLabel(),
-        const option::ProgressBarChars& char_frames = option::ProgressBarChars())
+    VProgressBarOptions(const option::Label& label = option::Label(),
+                        const option::CompletedLabel& completed_label = option::CompletedLabel(),
+                        const option::ProgressBarChars& char_frames = option::ProgressBarChars())
         : progress_label(label.progress_label),
           completed_label(completed_label.completed_label),
           char_frames(char_frames.progress_bar_chars) {}
